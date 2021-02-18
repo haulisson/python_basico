d68a23c209141ad3316e59e31b44c187d8269f3a
haulisson <haulisson.costa@ifrn.edu.br>
2021-02-17 21:57:32 -0300

    primeiro commit

---
+++ hello.py
@@ -0,0 +1,12 @@
+import kivy
+
+from kivy.app import App
+from kivy.uix.button import Label
+
+class HelloApp(App):
+    def build(self):
+        return Label(text = 'Hello World')
+
+if __name__ == "__main__":
+    HelloApp().run()
+    
